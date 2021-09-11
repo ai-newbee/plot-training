8040aacb5c7ecc4f693314b6dba9a2f964815def
dev <dev@qq.com>
2021-09-12 02:17:09 +0800

    bug fixs

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,8 +3,9 @@ package config
 const (
 	PojectRoot = "/home/dev/projects/dl-base"
 
-	StaticFolderName     = "static"
-	AssetFolderName      = StaticFolderName + "/assets"
+	StaticFolderName = "docs"
+	AssetFolderName  = StaticFolderName + "/assets"
+
 	Filename             = "samples.json"
 	SampleFilePath       = PojectRoot + "/" + AssetFolderName + "/" + Filename // /home/u/static/assets/samples.json
 	IterScalar           = 500
